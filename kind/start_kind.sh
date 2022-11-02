ddc2ef95f76131b0a3d95e3b141cf05a93f2b523
giusdp <dpl.gsp@tutanota.com>
2022-11-02 18:22:15 +0100

    feat(kind): add kind deploy

---
+++ kind/start_kind.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+
+KIND_NODE_TAG=v1.25.2
+
+# Boot cluster
+kind create cluster --config "$SCRIPTDIR/kind-cluster.yml" --image kindest/node:${KIND_NODE_TAG} --wait 10m || exit 1
+
+echo "Kubernetes cluster is deployed and reachable"
+kubectl cluster-info --context kind-funless-cluster
